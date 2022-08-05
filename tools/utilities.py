e8ce03ad3b9f947ee900b4739bf46632585b6942
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-05 22:28:08 +0700

    Update utilities.py

--- tools/utilities.py
+++ tools/utilities.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 def load_css():
-    with open("style.css") as f:
+    with open("tools/style.css") as f:
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
