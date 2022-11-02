03941d362853f83442d63a7579a04c6ce737d81f
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-02 13:44:06 +0700

    Update 1_🍎_Apples.py

--- pages/1_🍎_Apples.py
+++ pages/1_🍎_Apples.py
@@ -11,5 +11,3 @@ Fusce ultrices tincidunt elit imperdiet dapibus. Nunc condimentum fermentum laci
 
 Duis ultricies, sem eu fermentum fermentum, nisl tellus egestas odio, at placerat augue quam eget justo. Nullam facilisis porttitor ante, vitae blandit odio dapibus at. Nam tellus arcu, tristique sit amet posuere ut, posuere id nibh. Fusce ut diam dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc metus sapien, feugiat sed diam in, facilisis sollicitudin eros. Quisque quis ex et nunc pellentesque vestibulum. Praesent interdum molestie orci, id faucibus lorem blandit consequat. Vivamus aliquam, erat eu facilisis lacinia, felis nunc tristique lectus, iaculis pretium lectus massa eget risus. Pellentesque rhoncus arcu vel elementum venenatis. Ut in blandit libero, eget facilisis magna. Morbi aliquam elit ut neque imperdiet scelerisque.
 ''')
-
-test()
