dffbeb2cf67792705001f7e8c5dabf06ddcbf7f7
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-02 19:26:13 +0700

    Update and rename 3_🍐_Pear.py to 3_3️⃣_Pear.py

--- pages/3_3️⃣_Pear.py
+++ pages/3_3️⃣_Pear.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from utilities import load_css
 
-st.set_page_config(page_title="Pears", page_icon="🍐")
+st.set_page_config(page_title="Pears", page_icon="3️⃣")
 
-st.header("Pears 🍐")
+st.header("3️⃣ Pears 🍐")
 
 st.markdown('''
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.
