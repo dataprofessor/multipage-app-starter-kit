6f3650255ed7fa5dc88a08bee90ab7cd998f1d60
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-02 19:21:16 +0700

    Update and rename 1_1_Apples.py to 1_1️⃣_Apples.py

--- pages/1_1️⃣_Apples.py
+++ pages/1_1️⃣_Apples.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from utilities import load_css
 
-st.set_page_config(page_title="Apples", page_icon="1")
+st.set_page_config(page_title="Apples", page_icon="1️⃣")
 
 st.header("Apples 🍎")
 
