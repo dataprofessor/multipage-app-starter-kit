ec44d2be26877e062d5e2484e3148097c2377f4d
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-02 19:23:51 +0700

    Update and rename 2_🍊_Oranges.py to 2_2️⃣_Oranges.py

--- pages/2_2️⃣_Oranges.py
+++ pages/2_2️⃣_Oranges.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from utilities import load_css
 
-st.set_page_config(page_title="Oranges", page_icon="🍊")
+st.set_page_config(page_title="Oranges", page_icon="2️⃣")
 
-st.header("Oranges 🍊")
+st.header("2️⃣ Oranges 🍊")
 
 st.markdown('''
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.
