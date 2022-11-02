e0c3ff9f54890b1d049ed73242855e21e3d41b20
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-11-02 19:31:06 +0700

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -6,7 +6,7 @@ st.set_page_config(
     page_icon="🏠",
 )
 
-st.header("Welcome to the Multipage App! 👋")
+st.header("Welcome! 👋")
 st.write('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.')
 
 load_css()
